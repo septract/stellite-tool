33667ae44b39956027af33ae3823469e57521f65
Mike Dodds <mike.dodds@gmail.com>
2016-05-06 13:23:17 +0100

    Deleted redundant code

--- main.fs
+++ main.fs
@@ -1,22 +1,10 @@
 ﻿module Main
 
 open System
-
 open FParsec 
-open FParsec.CharParsers
 
 open Stellite.parser
 open Stellite.translator 
-
-//let testfile = "../../sample.stl"
-//let maintest argv = 
-//    let fg = freshGen () in  
-//    match (parseFile fg) testfile with 
-//    | Success(result,_,_) -> 
-//        let res = dispExec result 
-//        for l in res do printfn "%s" l
-//        0 
-//    | Failure(errorMsg,_,_) -> printfn "Failure %s" errorMsg; 1 
 
 [<EntryPoint>]
 let main argv = 
@@ -33,4 +21,4 @@ let main argv =
                     | Failure(errorMsg,_,_) -> eprintfn "Failure %s" errorMsg; 1 
             | _ -> printfn "One argument expected"; 1
     with 
-    | :? System.IO.FileNotFoundException -> printfn "Couldn't find the specified file!"; 1 
+    | :? System.IO.FileNotFoundException -> eprintfn "Couldn't find the specified file!"; 1 
