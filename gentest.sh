997fb6db8de5291e8f32caf0b4ef213a6bd6df0a
Mike Dodds <mike.dodds@gmail.com>
2016-05-09 13:26:11 +0100

    tweaks to test scripts

--- gentest.sh
+++ gentest.sh
@@ -6,6 +6,6 @@ then
 	exit 1
 fi
 
-touch $2 || exit 1;  
+touch $2 || exit 1; # check that the file is accessible  
 ./stellite.sh $1 > $2 || { echo "Predicate generation failed!">&2 ; exit 1; } 
 cat ./alloy/checkTemplate.als >> $2 
--- test.sh
+++ test.sh
@@ -2,15 +2,15 @@
 
 for file in $(find ./examples/{pass,fail} -iname '*.stl' | sort);
 do
-  printf "Checking $file... "
+  printf ">> $file... "
   name=`basename $file .stl`
   TMPFILE=`mktemp /tmp/${name}.XXXXXX` || \
       { echo "Couldn't create temporary file!">&2; exit 1; } 
   ./runtest.sh $file > $TMPFILE || exit 1 
   if grep -q "OUTCOME" $TMPFILE; then 
-       printf "passed\n";  
+       printf "pass\n";  
   else 
-       printf "failed\n"; 
+       printf "fail\n"; 
   fi  
 done
 
