3722de60b4171c0daca7925df532f782065aa3c5
Mike Dodds <mike.dodds@gmail.com>
2016-06-03 15:19:36 +0100

    script to generate, but not test, all the files.

---
+++ genall.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+for file in $(find ./examples/{"",vbcmn15/}{pass,fail} -iname '*.stl' | sort);
+do
+  # printf "Stellite> $file... "
+  name=`basename $file .stl`
+  outfile="./alloy/gen/${name}.als"
+
+  # Run the test. 
+  STARTTIME=$(date +%s)
+  echo "Stellite> generating ${outfile}"
+  ./gentest.sh ${1-7} $file $outfile || exit 1 
+  ENDTIME=$(date +%s)
+done
+
