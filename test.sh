035940e33c4cb175ac7b6cee09164e54f372e21b
Mike Dodds <mike.dodds@gmail.com>
2016-06-08 18:18:26 +0100

    added timeout feature to test.sh

--- test.sh
+++ test.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
+TIMEOUT=3600
+
 for file in $(find ./examples/{"",vbcmn15/}{pass,fail} -iname '*.stl' | sort);
 do
-  # printf "Stellite> $file... "
   name=$(basename $file .stl)
   TMPFILE=$(mktemp /tmp/${name}.XXXXXX) || \
       { echo "Couldn't create temporary file!">&2; exit 1; } 
 
+  printf "Stellite> %-40s " $file;
+
   # Run the test. 
   STARTTIME=$(date +%s)
-  ./runtest.sh ${1-7} $file > $TMPFILE || exit 1 
+  timeout --foreground $TIMEOUT ./runtest.sh ${1-7} $file > $TMPFILE || exit 1
+  CMDRES=$?
   ENDTIME=$(date +%s)
 
   # Check the outcome
-  if grep -q "OUTCOME" $TMPFILE; then 
-       printf "Stellite> %-40s pass" $file;  
+  if [ $CMDRES -eq 124 ]; then 
+    printf "Timed out\n"; 
+  elif grep -q "OUTCOME" $TMPFILE; then 
+    printf "Pass";  
+    printf " ($(($ENDTIME - $STARTTIME)) secs)\n"
   else 
-       printf "Stellite> %-40s fail" $file;  
+    printf "Fail";  
+    printf " ($(($ENDTIME - $STARTTIME)) secs)\n"
   fi  
-  printf " ($(($ENDTIME - $STARTTIME)) secs)\n"
   
   # Detect Alloy warnings 
   if grep -q "Warning:" $TMPFILE; then 
